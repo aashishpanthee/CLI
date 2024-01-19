5093adc5c0641a455f062d8c218efc9d0807034f
Aashish Panthee <aashishpanthee060@gmail.com>
2024-01-19 17:41:53 +0545

    Modified index.tsx to build & add custom plugin. -Remove transform to change input text from 'jsx' to 'es5' -Add build method to change input text from 'jsx' to 'es5'
    --Build method is used for transpiling and bundling the plugin
    --Build method is called by the plugin to transpile the input text
    --Tranform method is only used for transpiling the input text. It is not used for bundling the plugin.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as esbuild from "esbuild-wasm";
 import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom/client";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
   const [input, setInput] = useState<string>("");
@@ -21,11 +22,14 @@ const App = () => {
     if (!ref.current) {
       return;
     }
-    const result = await ref.current.transform(input, {
-      loader: "jsx",
-      target: "es2015",
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
     });
-    setCode(result.code);
+
+    setCode(result.outputFiles[0].text);
   };
 
   return (
